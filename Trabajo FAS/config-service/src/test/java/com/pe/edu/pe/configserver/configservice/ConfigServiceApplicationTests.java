ffb13cd59dd22c6699fcadbf919561500feb2eb6
Gonzalo Farfan <gfarfan595@gmail.com>
2020-11-16 16:21:33 -0500

    Agregado de service a configuracion

---
+++ Trabajo FAS/config-service/src/test/java/com/pe/edu/pe/configserver/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.edu.pe.configserver.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
